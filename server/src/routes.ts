ab8e0846108b7314c3ddc1fd62ffdb1560f011f9
Bruno Camba <brunocamba@gmail.com>
2020-08-04 13:53:28 -0300

    Added initial backend

---
+++ server/src/routes.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import ClassesController from './controllers/classesController';
+import ConnectionsController from './controllers/connectionsController';
+
+const routes = express.Router();
+const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsController();
+
+routes.post('/classes', classesControllers.create);
+routes.get('/classes', classesControllers.index);
+routes.post('/connections', connectionsControllers.create);
+routes.get('/connections', connectionsControllers.index);
+
+export default routes;
