2919b179ac3bf67d5b9956c2215bb9ddca32e3bc
Bruno Camba <brunocamba@gmail.com>
2020-08-05 22:28:29 -0300

    Integrated backend with frontend

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -8,10 +8,21 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import teachIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import api from '../../services/api';
+
 import './styles.css'
 
-
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -39,7 +50,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          200 connections already established <img src={purpleHeartIcon} alt="Purple heart" />
+          {totalConnections} connections already established <img src={purpleHeartIcon} alt="Purple heart" />
         </span>
       </div>
     </div>
